583b07d421809ce077c0ca1059729377c7bbf7c1
Yousuf Al Azad <yousufalazad@gmail.com>
2024-09-10 18:36:24 +0100

    DB update

--- database/migrations/2024_06_21_182514_create_org_administrators_table.php
+++ database/migrations/2024_06_21_182514_create_org_administrators_table.php
@@ -10,19 +10,22 @@ return new class extends Migration
     {
         Schema::create('org_administrators', function (Blueprint $table) {
             $table->id();
-             // Foreign key to 'users' table
-             $table->foreignId('org_user_id')
-             ->constrained('users')
-             ->onDelete('cascade'); // Cascade on delete to remove associated addresses when user is deleted
 
-             $table->foreignId('individual_user_id')
-             ->constrained('users')
-             ->onDelete('cascade'); 
+            // Foreign key to 'users' table
+            $table->foreignId('org_user_id')
+                ->constrained('users')
+                ->onDelete('cascade'); // Cascade on delete to remove associated addresses when user is deleted
+
+            // Foreign key to 'users' table for individual users
+            $table->foreignId('individual_user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
             $table->date('from_date')->nullable();
             $table->date('end_date')->nullable(); //Automatic date capture when change to new individual ID, can't editable (PUT not considerable), always new created
             $table->tinyInteger('status')->nullable()->default(1);
             $table->timestamps();
-                });
+        });
     }
 
     public function down(): void
