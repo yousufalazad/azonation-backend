aad6264287e400f39fa18b0182a92ffe1ea27e69
Yousuf Al Azad <ourazad@gmail.com>
2024-10-09 21:54:45 +0100

    Account Fund Type Added, office record mcr and orgReport mcr

--- database/migrations/2024_10_08_210909_create_org_office_records_table.php
+++ database/migrations/2024_10_08_210909_create_org_office_records_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('org_office_records', function (Blueprint $table) {
             $table->id();
+            // Foreign key referencing the users table (creator or recipient of the plan)
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade')
+                ->comment('Creator or responsible user for the transaction.');
+
+            $table->string('title', 255);
+            $table->string('description', 255);
+            $table->string('document', 255);
+
+            // Foreign key referencing the privacy setups table (privacy settings)
+            $table->foreignId('privacy_setup_id')
+            ->constrained('privacy_setups')
+            ->onDelete('cascade')
+            ->comment('Privacy level of the year plan (e.g., public, private, only members).');
+
             $table->timestamps();
         });
     }
