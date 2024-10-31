035a05c8b50cbbea785ce16ff07d44456a927b8c
Yousuf Al Azad <ourazad@gmail.com>
2024-10-31 21:29:32 +0000

    Package GET complete for org user

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -8,4 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 class Package extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'max_members',
+        'storage_limit',
+        'custom_branding',
+        'api_access',
+        'priority_support',
+        'meeting_limit',
+        'event_limit',
+        'project_limit',
+        'asset_limit',
+        'document_limit',
+        'report',
+        'advanced_report',
+        'custom_report',
+        'support',
+        'premium_support',
+        'dedicated_account_manager',
+        'custom_domain',
+        'custom_email_template',
+        'multi_currency_payment',
+        'custom_username',
+        'web_profile',
+        'status'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
 }
