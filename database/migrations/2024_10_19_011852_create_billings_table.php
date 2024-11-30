275d1538e4a8b583490f45f0984a9982562fbbe5
Yousuf Al Azad <yousufalazad@gmail.com>
2024-11-30 06:32:08 +0000

    Privacy setup api change for SuperAdmin, meetings and billing update

--- database/migrations/2024_10_19_011852_create_billings_table.php
+++ database/migrations/2024_10_19_011852_create_billings_table.php
@@ -15,47 +15,57 @@ return new class extends Migration
         Schema::create('billings', function (Blueprint $table) {
             $table->id();
 
-            // Unique Invoice ID
-            $table->string('billing_code', 15)->unique()->comment('Unique 15-character alphanumeric transaction ID with prefix AZON-BILL.');
+            // Unique billing identifier
+            //$table->string('billing_code', 15)->unique()->comment('Unique 15-character alphanumeric transaction ID with prefix AZON-BILL.');
+            //$table->string('billing_code', 15)()->comment('Unique 15-character alphanumeric transaction ID with prefix AZON-BILL.');
 
             // Foreign key linking to the 'users' table
             $table->foreignId('user_id')
-                ->nullable()
                 ->constrained()
-                ->onDelete('set null')
+                ->onDelete('cascade')
                 ->comment('References the user for billing. Null if user is deleted.');
 
             // Store user details statically for historical reference
-            $table->string('user_name')
+            $table->string('user_name', 255)
                 ->nullable()
                 ->comment('User name snapshot for billing reference');
+
+            
+
+                // Service and billing month details
+            $table->string('service_month')->comment('Month in which the service was consumed');
+            $table->string('billing_month')->comment('Month in which the bill is generated');
+
+            $table->tinyInteger('service_year', 4)
+                ->nullable()
+                ->comment('Year for billing reference');
+            
+            $table->tinyInteger('billing_year', 4)
+                ->nullable()
+                ->comment('Year for billing reference');
 
             // Brief description of the bill
             $table->string('description', 255)
             ->nullable()
             ->comment('Optional detailed description of the bill.');
 
-            $table->string('billing_address')
+            $table->string('billing_address', 255)
                 ->nullable()
                 ->comment('User billing address snapshot for reference');
 
             // Billing item details
-            $table->string('item_name')->comment('Name or description of the billed item or service');
+            $table->string('item_name', 255)->comment('Name or description of the billed item or service');
 
             // Billing period
             $table->date('period_start')->comment('Billing period start date');
             $table->date('period_end')->comment('Billing period end date');
 
-            // Service and billing month details
-            $table->string('service_month')->comment('Month in which the service was consumed');
-            $table->string('billing_month')->comment('Month in which the bill is generated');
-
             // Member count data from active_member_counts table
-            $table->integer('active_member_count')->default(0)->comment('Daily total active members within billing period');
-            $table->integer('billable_active_member_count')->default(0)->comment('Total billable active members within billing period');
+            $table->integer('active_member_count', 10)->comment('Daily total active members within billing period');
+            $table->integer('billable_active_member_count', 10)->comment('Total billable active members within billing period');
 
             // Billing rate and calculation
-            $table->decimal('member_daily_rate', 10, 2)->comment('Daily rate per active member');
+            $table->decimal('daily_price_rate', 10, 2)->comment('Daily rate per active member');
             $table->decimal('total_bill_amount', 10, 2)->comment('Total bill amount based on rate, members, and days');
 
             // Billing status and administrative note
@@ -63,7 +73,7 @@ return new class extends Migration
                 ->default('issued')
                 ->comment('Billing status: issued, unissued, pending, cancelled, or draft');
 
-            $table->string('admin_notes', 100)->nullable()->comment('Administrative notes for reference');
+            $table->string('admin_notes', 255)->nullable()->comment('Administrative notes for reference');
 
             // Active or inactive status flag
             $table->boolean('is_active')->default(true)->comment('Indicates if the billing record is active');
