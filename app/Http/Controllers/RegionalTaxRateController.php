42b58340a1bea4bd94cffe73580a825bce815784
Yousuf Al Azad <yousufalazad@gmail.com>
2024-12-10 06:13:27 +0000

    Regional Tax Rate -mcr added

---
+++ app/Http/Controllers/RegionalTaxRateController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RegionalTaxRate;
+use Illuminate\Http\Request;
+
+class RegionalTaxRateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RegionalTaxRate $regionalTaxRate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RegionalTaxRate $regionalTaxRate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RegionalTaxRate $regionalTaxRate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RegionalTaxRate $regionalTaxRate)
+    {
+        //
+    }
+}
