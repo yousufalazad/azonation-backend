f61a80aa064d169ff98926e52d4b57f905d3c086
Yousuf Al Azad <ourazad@gmail.com>
2024-12-23 20:42:00 +0000

    Cart, Cart Item, and Order Status added

---
+++ app/Http/Controllers/OrderStatusController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrderStatus;
+use Illuminate\Http\Request;
+
+class OrderStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OrderStatus $orderStatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OrderStatus $orderStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OrderStatus $orderStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OrderStatus $orderStatus)
+    {
+        //
+    }
+}
