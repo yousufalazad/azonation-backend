07954d7cf516d1c35e44e8a7a27339a6d72ff1dc
Yousuf Al Azad <ourazad@gmail.com>
2025-01-12 22:44:15 +0000

    Payment -mcr added, invoices and orders updated

--- database/migrations/2024_10_19_011920_create_invoices_table.php
+++ database/migrations/2024_10_19_011920_create_invoices_table.php
@@ -31,26 +31,36 @@ return new class extends Migration
 
             $table->string('user_name', length: 100)->nullable()->comment('User name snapshot for billing reference');
 
+            // Financial fields
+            $table->decimal('sub_total', 10, 2)->comment('Total amount for the order');
+            $table->decimal('discount_amount', 10, 2)->default(0)->nullable()->comment('Discount applied to the order, if any');
+            $table->decimal('shipping_cost', 10, 2)->nullable()->comment('Cost of shipping');
+            $table->decimal('total_tax', 10, 2)->nullable()->comment('Total tax applied to the order');
+            $table->decimal('credit_applied', 10, 2)->default(0)->comment('Credit amount applied from the user’s balance');
+            $table->decimal('total_amount', 10, 2)->comment('Final amount due after discounts, credit and taxes');
+            $table->decimal('amount_paid', 10, 2)->default(0)->nullable()->comment('The total amount paid towards the invoice so far.');
+            $table->decimal('balance_due', 10, 2)->comment('The remaining amount still owed.');
+
+            $table->string('discount_title', 50)->nullable()->comment('Title or description of the discount');
+            $table->decimal('tax_rate', 10, 2)->nullable()->comment('Total tax applied to the order');
+            $table->string('currency', 3)->default('USD')->comment('Currency code for the order amount');
+            $table->string('coupon_code')->nullable()->comment('Coupon code used for the order, if applicable');
+            $table->string('payment_method', length: 20)->comment('Payment method for the invoice, e.g., PayPal, Cash on Delivery');
+
+
             // Address fields
             $table->string('shipping_address')->comment('Shipping address for the order');
             $table->string('billing_address')->comment('Billing address for the order');
+            $table->enum('shipping_status', ['pending', 'shipped', 'delivered'])->default('pending')->comment('Status of shipping');
 
-            $table->string('payment_method', length: 20)->comment('Payment method for the invoice, e.g., PayPal, Cash on Delivery');
+            // Additional fields
+            $table->string('user_country')->comment('Country of the user');
+            $table->string('user_region')->comment('Billing region of the user');
 
             // Date columns
             $table->date('generated_at')->nullable()->comment('Date when the invoice was generated');
             $table->date('issued_at')->nullable()->comment('Date when the invoice was issued');
             $table->date('due_at')->nullable()->comment('Payment due date');
-
-
-            // Financial details
-            $table->string('currency', length: 3)->nullable()->comment('Currency on service month');
-            $table->decimal('subtotal', 10, 2)->nullable()->comment('Subtotal before discounts and taxes, data comes from billings table - bill_amount column');
-            $table->string('discount_title', 50)->nullable()->comment('Title or description of the discount');
-            $table->decimal('discount', 10, 2)->default(0)->comment('Discount amount applied to the invoice');
-            //$table->decimal(column: 'tax_amount', 10, 2)->default(0)->comment('Total tax applied');
-            $table->decimal('credit_applied', 10, 2)->default(0)->comment('Credit amount applied from the user’s balance');
-            $table->decimal('balance', 10, 2)->default(0)->comment('Final amount due after discounts, credit and taxes');
 
             // Additional fields
             $table->string('invoice_note', 100)->nullable()->comment('Notes for the invoice, like Non-refundable');
