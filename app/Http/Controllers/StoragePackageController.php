d7862149e20ec12a23e86c535bea7d2c3dfb676c
Yousuf Al Azad <yousufalazad@gmail.com>
2025-01-17 22:31:27 +0000

    Pricing, subscription, package and tax new name -mcr

---
+++ app/Http/Controllers/StoragePackageController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StoragePackage;
+use Illuminate\Http\Request;
+
+class StoragePackageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StoragePackage $storagePackage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StoragePackage $storagePackage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, StoragePackage $storagePackage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StoragePackage $storagePackage)
+    {
+        //
+    }
+}
