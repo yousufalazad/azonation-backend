a727b5ff608880e7178e19c0df3b8226eea6da57
Yousuf Al Azad <yousufalazad@gmail.com>
2025-01-24 16:10:31 +0000

    Billing update

--- database/migrations/2025_01_14_222943_create_storage_billings_table.php
+++ database/migrations/2025_01_14_222943_create_storage_billings_table.php
@@ -13,27 +13,27 @@ return new class extends Migration
     {
         Schema::create('storage_billings', function (Blueprint $table) {
             $table->id();
-            $table->string('storage_billing_code', 15)->nullable()->unique()->comment('Unique 15-character alphanumeric transaction ID with prefix AZON-STO.');
+            // $table->string('storage_billing_code', 15)->nullable()->unique()->comment('Unique 15-character alphanumeric transaction ID with prefix AZON-STO.');
             
-            $table->foreignId('user_id')
-                ->nullable()
-                ->constrained('users')
-                ->onDelete('cascade')
-                ->comment('References the user for billing. Null if user is deleted.');
+            // $table->foreignId('user_id')
+            //     ->nullable()
+            //     ->constrained('users')
+            //     ->onDelete('cascade')
+            //     ->comment('References the user for billing. Null if user is deleted.');
                 
-            $table->string('user_name', length: 255)->nullable()->comment('User name snapshot for billing reference');
+            // $table->string('user_name', length: 255)->nullable()->comment('User name snapshot for billing reference');
 
-            //Service and billing month details
-            $table->string('service_month_year', 20)->nullable()->comment('Month in which the service was consumed');
-            $table->string('billing_month_year', 20)->nullable()->comment('Month in which the bill is generated');
-            // $table->smallInteger('service_year')->nullable()->comment('Year for billing reference');
-            // $table->smallInteger('billing_year')->nullable()->comment('Year for billing reference');
+            // //Service and billing month details
+            // $table->string('service_month_year', 20)->nullable()->comment('Month in which the service was consumed');
+            // $table->string('billing_month_year', 20)->nullable()->comment('Month in which the bill is generated');
+            // // $table->smallInteger('service_year')->nullable()->comment('Year for billing reference');
+            // // $table->smallInteger('billing_year')->nullable()->comment('Year for billing reference');
  
 
-            //Storage billing
-            $table->string('storage_package_name', length: 15)->nullable()->comment('Storage package name on service month');
-            $table->decimal('storage_price_rate', 10, 2)->nullable()->comment('Storage rate per day');
-            $table->decimal('storage_sub_total', 10, 2)->nullable()->comment('Storage bill amount based on rate, days, and total members');
+            // //Storage billing
+            // $table->string('storage_package_name', length: 15)->nullable()->comment('Storage package name on service month');
+            // $table->decimal('storage_price_rate', 10, 2)->nullable()->comment('Storage rate per day');
+            // $table->decimal('storage_sub_total', 10, 2)->nullable()->comment('Storage bill amount based on rate, days, and total members');
             
             //$table->integer('storage_days')->nullable()->comment('Number of days for storage');
             //$table->decimal('storage_tax', 10, 2)->default(0)->comment('Total tax applied to storage bill');
