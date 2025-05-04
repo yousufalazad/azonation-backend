62ead48e70a06cad6bb3a537f2e32b460b5f37d0
Yousuf Al Azad <yousufalazad@gmail.com>
2025-05-04 20:18:06 +0100

    Trigger deploy

--- public/index.php
+++ public/index.php
@@ -4,11 +4,22 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-if (file_exists($maintenance = __DIR__.'/../api/storage/framework/maintenance.php')) {
-    require $maintenance;
+// Maintenance mode check
+if (file_exists(__DIR__ . '/api/storage/framework/maintenance.php')) {
+    require __DIR__ . '/api/storage/framework/maintenance.php';
 }
 
-require __DIR__.'/../api/vendor/autoload.php';
+// Autoloader
+require __DIR__ . '/api/vendor/autoload.php';
 
-(require_once __DIR__.'/../api/bootstrap/app.php')
-    ->handleRequest(Request::capture());
+// Bootstrap the application
+$app = require_once __DIR__ . '/api/bootstrap/app.php';
+
+// Handle the request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+$response->send();
+
+$kernel->terminate($request, $response);
