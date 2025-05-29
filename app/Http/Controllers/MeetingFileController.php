c8d3415526f30db5ef19535755f4775713eb5657
Yousuf Al Azad <yousufalazad@gmail.com>
2025-05-29 01:33:49 +0100

    Image mcr added for Meeting, Event and Project

---
+++ app/Http/Controllers/MeetingFileController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MeetingFile;
+use Illuminate\Http\Request;
+
+class MeetingFileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MeetingFile $meetingFile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MeetingFile $meetingFile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MeetingFile $meetingFile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MeetingFile $meetingFile)
+    {
+        //
+    }
+}
