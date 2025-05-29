7b2aa243ea3fdb554cd98555adb2a3dcd5604e89
Yousuf Al Azad <yousufalazad@gmail.com>
2025-05-29 01:33:49 +0100

    Image mcr added for Meeting, Event and Project

---
+++ app/Http/Controllers/ProjectFileController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProjectFile;
+use Illuminate\Http\Request;
+
+class ProjectFileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProjectFile $projectFile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProjectFile $projectFile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProjectFile $projectFile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProjectFile $projectFile)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_05_29_002759_create_event_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_images', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')
+                ->constrained('events')
+                ->onDelete('cascade')
+                ->comment('Image path relational table with events table.');
+
+            $table->string('image_path')->comment('The storage path or URL of the profile image');
+            $table->string('file_name')->nullable()->comment('The original file name of the uploaded image');
+            $table->string('mime_type')->nullable()->comment('The MIME type of the image file, e.g., image/jpeg or image/png');
+            $table->integer('file_size')->nullable()->comment('The size of the image file in kilobytes (KB)');
+            $table->boolean('is_public')->default(true)->comment('Whether the profile image is publicly visible or not');
+            $table->boolean('is_active')->default(true); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_images');
+    }
+};
