a7e5c0326a7a203375140ced4ab0baaa50062f39
Yousuf Al Azad <yousufalazad@gmail.com>
2025-07-25 00:25:07 +0100

    Gateway credential mcr added

---
+++ app/Http/Controllers/GatewayCredentialController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GatewayCredential;
+use Illuminate\Http\Request;
+
+class GatewayCredentialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(GatewayCredential $gatewayCredential)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(GatewayCredential $gatewayCredential)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, GatewayCredential $gatewayCredential)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(GatewayCredential $gatewayCredential)
+    {
+        //
+    }
+}
