7583d73bb9287bffd663cf63d9ccca2cc8906a0a
Yousuf Al Azad <yousufalazad@gmail.com>
2025-08-26 20:32:11 +0100

    Membership termination mcr added

---
+++ app/Http/Controllers/Org/Membership/MembershipTerminationReasonController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Org\Membership;
+
+use App\Http\Controllers\Controller;
+
+use App\Models\MembershipTerminationReason;
+use Illuminate\Http\Request;
+
+class MembershipTerminationReasonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MembershipTerminationReason $membershipTerminationReason)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MembershipTerminationReason $membershipTerminationReason)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MembershipTerminationReason $membershipTerminationReason)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MembershipTerminationReason $membershipTerminationReason)
+    {
+        //
+    }
+}
