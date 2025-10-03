95bfdc087781e63ccab6b8bebe607d7300d06167
Yousuf Al Azad <ourazad@gmail.com>
2025-10-03 02:57:13 +0100

    Google auth in deployment issue fixed

--- config/cors.php
+++ config/cors.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login'],
+    'paths' => ['api/*', 'auth/*', 'sanctum/csrf-cookie', 'login'],
     'allowed_methods' => ['*'],
-    'allowed_origins' =>  ['https://my.azonation.com', 'http://localhost:5173'],
+    'allowed_origins' =>  ['https://app.azonation.com', 'http://localhost:5173', 'http://127.0.0.1:5173'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
